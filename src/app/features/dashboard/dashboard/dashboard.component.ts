c969b90b5c9d578c932667cc79a58573c7492145
vinslin <vinslinpackia@gmail.com>
2025-08-02 16:17:56 +0530

    dashboard initial completed

--- src/app/features/dashboard/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard/dashboard.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
+  categoryTemp = 10;
+  product = 11;
+  bill = 12;
 
+  goto() {}
 }
